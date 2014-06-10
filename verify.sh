8b552956918614ee2a09dc55f1b6633180fd2dfb
oxygen XML editor <oxygen@rhodes.holycross.edu>
2014-06-10 15:10:03 -0400

    Update configs for VM.

--- verify.sh
+++ verify.sh
@@ -1,3 +1,5 @@
 echo "Verifying from configuration in /vagrant/hmt-iliad10/vm-mom-config.gradle verify"
 
+cd /vagrant/hmt-mom
+
 gradle clean && gradle -Pconf=/vagrant/hmt-iliad10/vm-mom-config.gradle verify
