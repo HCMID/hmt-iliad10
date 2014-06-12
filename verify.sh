4791294c7a6f404759431b9f39a10c61d8d5bcb4
Neel Smith <dnsmith.neel@gmail.com>
2014-06-12 13:49:36 -0400

    Update verify script

--- verify.sh
+++ verify.sh
@@ -1,3 +1,18 @@
-echo "Verifying from configuration in /vagrant/hmt-iliad10/vm-mom-config.gradle verify"
 
-gradle clean && gradle -Pconf=/vagrant/hmt-iliad10/vm-mom-config.gradle verify
+if [ "$#" -ne 1 ]; then
+    echo "Usage: sh verify.sh URN"
+    exit
+fi
+
+
+echo "Verifying from configuration in /vagrant/hmt-iliad11/vm-mom-config.gradle verify"
+
+
+cd /vagrant/hmt-mom
+
+echo "Cleaning previous HMT MOM results..."
+gradle clean
+
+echo Beginning verification for folio $1
+
+gradle -Pfolio=$1 -Pconf=/vagrant/hmt-iliad10/vm-mom-config.gradle verify
